95b09f29f27546f85758b339375ba17cdcbba0bf
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-09-21 16:50:51 -0500

    add cookie session 1.0

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,58 @@
 import express from "express";
-const morgan = require("morgan");
 import cookieParser from "cookie-parser";
 
 const app = express();
-const port = 3000;
-// Middleware JSON
-app.use(express.json());
-//Middleware libreria externa
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms")
-);
-app.use(cookieParser());
+const port = 5500;
 
-// Routes
+app.use(cookieParser()); // Poblar req.cookies con objetos de cookies
+app.use(express.json()); // Transformar req.body a JSON
 
-app.get("/", (req, res) => {
-   console.log(req.cookies);
-  res.send(req.cookies);
+// Arreglo de usuarios en memoria:
+// El id será un string ya que usaremos 'crypto.randomUUID()' para generarlo
+const users: { id: string; email: string; password: string }[] = [];
+
+app.post("/signup", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((u) => u.email === email);
+
+  if (user) {
+    res.status(400).send("El correo ya está registrado");
+    return;
+  }
+  const newUser = {
+    id: crypto.randomUUID(),
+    email,
+    password,
+  };
+
+  users.push(newUser);
+  res.status(201).json(newUser);
 });
 
-app.get("/set-cookie", (_req, res) => {
-  const cookiePreferences = {
-    userId: 1,
-    language: "ES",
-  };
-  res.cookie("preferences", cookiePreferences, { maxAge: 20000, httpOnly: true });
-  res.cookie("theme", "dark");
-  res.send("Cookie seteada");
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((u) => u.email === email && u.password === password);
+  if (user) {
+    res.cookie("userId", user.id, { httpOnly: true });
+    res.send("Login exitoso");
+  } else {
+    res.status(401).send("Credenciales incorrectas");
+  }
+});
+
+app.get("/user", (req, res) => {
+  const userId = req.cookies.userId;
+  const user = users.find((u) => u.id === userId);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(403).send("Acceso denegado");
+  }
+});
+
+app.post("/logout", (_req, res) => {
+  res.clearCookie("userId");
+  res.send("Logout exitoso");
 });
 
 app.listen(port, () => {
